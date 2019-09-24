2a39b37aa5749629b239530eaf10f412cd9e9d34
Arthur Masson <arthur.sw@gmail.com>
2019-09-24 10:22:18 +0200

    changed repo name and added Circle CI

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('boutiques-gui-angular app is running!');
+    expect(page.getTitleText()).toEqual('boutiques-gui-front app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'boutiques-gui-angular'`, () => {
+  it(`should have as title 'boutiques-gui-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('boutiques-gui-angular');
+    expect(app.title).toEqual('boutiques-gui-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('boutiques-gui-angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('boutiques-gui-front app is running!');
   });
 });
